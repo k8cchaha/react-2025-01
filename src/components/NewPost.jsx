49599551a99fe77e5c871646ef5adc16a46b919f
Alex Wang <alexwcwang@kkcompany.com>
2025-02-11 02:52:56 +0800

    add event listener

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,9 +1,13 @@
 function NewPost() {
+    function changeBodyHandler(event) {
+        console.log(event.target.value)
+    }
+
     return (
         <div>
             <p>
             <label htmlFor="body">Text</label>
-            <textarea id="body" required rows={3} />
+            <textarea id="body" required rows={3} onChange={changeBodyHandler} />
             </p>
             <p>
                 <label htmlFor="name">Your Name</label>
