9e0d403ed9b3dfd6e024d647ca888c580801f778
Alex Wang <alexwcwang@kkcompany.com>
2025-02-11 09:59:34 +0800

    add state hook

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,14 +1,19 @@
+import {useState} from 'react'
+
 function NewPost() {
+    const [ enteredBody, setEnteredBody ] = useState('')
     function changeBodyHandler(event) {
         console.log(event.target.value)
+        setEnteredBody(event.target.value)
     }
 
     return (
         <div>
             <p>
-            <label htmlFor="body">Text</label>
-            <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+                <label htmlFor="body">Text</label>
+                <textarea id="body" required rows={3} onChange={changeBodyHandler} />
             </p>
+            { enteredBody }
             <p>
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" required />
