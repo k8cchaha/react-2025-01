24fe45807cdd5995a1cee693c561641697e240ea
Alex Wang <k8cchaha@gmail.com>
2025-03-05 18:34:34 +0800

    add sentry settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "kk-solution",
+    project: "javascript-react"
+  })],
+
   base: './',
+
+  build: {
+    sourcemap: true
+  }
 })
