0323896ab4465f14bd7af5f9241ac12666c1505d
Alex Wang <k8cchaha@gmail.com>
2025-03-06 15:03:23 +0800

    change error generated to parsing problem

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -23,9 +23,10 @@ function NewPost({onCancel, onAddPost}) {
         onCancel()
     }
 
-    function doVariableError() {
-        const aa = 0;
-        aa = 5;
+    function doParseError() {
+        const parseString = "{aa:12, bb:}";
+        const result = JSON.parse(parseString);
+        console.log(result)
     }
 
     return (
@@ -39,7 +40,7 @@ function NewPost({onCancel, onAddPost}) {
                 <input type="text" id="name" required onChange={authorChangeHandler} />
             </p>
             <button onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>
-            <button onClick={doVariableError}>Do Variable Error</button>
+            <button onClick={doParseError}>Do Parse Error</button>
             <p>
                 <button type='button' onClick={onCancel}>Cancel</button>
                 <button>Submit</button>
